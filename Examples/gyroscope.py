87b4bdd596e681fa586a42de75b5ad8b4340260f
CiaranMerritt <ciaran.merritt@colliewobble.com>
2024-09-19 16:07:19 +0100

    feat: Setting up examples of the different orientation sensors

---
+++ Examples/gyroscope.py
@@ -0,0 +1,19 @@
+from sense_hat import SenseHat
+import time
+# import only system from os
+from os import system
+
+sense = SenseHat()
+
+# alternatives
+minutes = 0
+seconds = 20
+t_end = time.time() + (60 * minutes) + seconds
+while time.time() < t_end:
+    # do whatever you do
+    system('clear')
+    gyro_only = sense.get_gyroscope()
+    print("p: {pitch}, r: {roll}, y: {yaw}".format(**gyro_only))
+    print(sense.gyro)
+    print(sense.gyroscope)
+    time.sleep(1)
