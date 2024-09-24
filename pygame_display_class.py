a6a9318fd85600372de9e3bace8f8a64920b32c0
CiaranMerritt <ciaran.merritt@colliewobble.com>
2024-09-24 09:42:47 +0100

    feat: Making a class that will handle starting and closing the pygames app

---
+++ pygame_display_class.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.locals import *
+
+class PygameDisplay(object):
+
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+
+    def __init__(self, game) -> None:
+        pygame.init()
+        pygame.font.init() 
+        self.game = game()
+        pass
+
+    def runngGame(self):
+        
+        running = True
+        dt = 0
+
+        while running:
+            # poll for events
+            # pygame.QUIT event means the user clicked X to close your window
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+
+            self.game.run()
+
+            # flip() the display to put your work on screen
+            pygame.display.flip()
+
+            # limits FPS to 60
+            # dt is delta time in seconds since last frame, used for framerate-
+            # independent physics.
+            dt = self.clock.tick(60) / 1000
+
+        pygame.quit()
---
+++ test.py
@@ -0,0 +1,35 @@
+from pygame_display_class import PygameDisplay
+
+# Example file showing a circle moving on screen
+
+import pygame
+from pygame.locals import *
+from sense_hat import SenseHat
+
+
+class test():
+    
+
+    def __init__(self) -> None:
+        self.font = pygame.font.Font(None, 20)
+        self.sense = SenseHat()
+        pass
+
+    def run(self):
+        # fill the screen with a color to wipe away anything from last frame
+        PygameDisplay.screen.fill("black")
+
+        orientation = self.sense.get_orientation()
+        pitch_text = self.font.render(f'Pitch: {orientation["pitch"]}', True, (255, 255, 255))
+        roll_text = self.font.render(f'Roll: {orientation["roll"]}', True, (255, 255, 255))
+        yaw_text = self.font.render(f'Yaw: {orientation["yaw"]}', True, (255, 255, 255))
+        PygameDisplay.screen.blit(pitch_text, (10, 30))
+        PygameDisplay.screen.blit(roll_text, (10, 50))
+        PygameDisplay.screen.blit(yaw_text, (10, 70))
+
+    def quit(self):
+        self.sense.clear()
+
+game = PygameDisplay(test)
+
+game.runngGame()
